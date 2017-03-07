c55e614ff2eb26696012b4cb613eb5eac383a5f9
Evgeny <b.ev@bk.ru>
2017-03-07 20:02:14 +0300

    .

--- Main.py
+++ Main.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
 import utils.plotter
 
 f = "C:\\data\\projects\\machineLearning\\kaggle\\competitions\\titanic\\"
 train = pd.read_csv(f + "train.csv")
-ptrain = pd.DataFrame()
+pData = pd.DataFrame()
 
-ptrain['Sex'] = train["Sex"].map({"male": 1, "female": 0}).astype(int)
-ptrain['Family'] = train['Parch'] + train['SibSp']
+print(train.head(3))
 
-ageTrainData = train[["Pclass", "SibSp", "Parch", "Fare", "Age","Survived"]]
+pData["Pclass"] = train["Pclass"]
+pData["Fare"] = train["Fare"]
+pData['Sex'] = train["Sex"].map({"male": 1, "female": 0}).astype(int)
+pData['Family'] = train['Parch'] + train['SibSp']
+pData["Survived"] = train["Survived"]
+
+ageTrainData = train[["Pclass", "SibSp", "Parch", "Fare", "Age", "Survived"]]
 nan_age = np.isnan(ageTrainData["Age"])
 ageTrainData = ageTrainData.dropna()
 
@@ -30,5 +40,56 @@ print(clf.score(ageX.values, ageY.values))
 
 exp = ageY.values
 predicted = clf.predict(ageX.values)
+# utils.plotter.plot_predict_accuracity(clf.predict(ageX.values), ageY.values)
 
-utils.plotter.plot_predict_accuracity(clf.predict(ageX.values), ageY.values)
+res = clf.predict(ageX.values)
+pData["Age"] = train["Age"]
+
+oldValues = pData["Age"].dropna().values
+
+# X1 = ptrain.dropna()["Fare"].values
+# Y1 = ptrain.dropna()["Age"].values;
+
+i = 0
+ageValues = pData["Age"].values
+for index, val in enumerate(ageValues):
+    if np.isnan(val):
+        ageValues[index] = res[i]
+        i += 1
+pd.DataFrame.replace(pData["Age"], list(ageValues))
+
+# utils.plotter.show_two_hist(ptrain["Age"].dropna().values, oldValues)
+print(pData.head(15))
+print("Has nan values={0}".format(len(pData) > len(pData.dropna())))
+
+
+
+# CLASSIFICATION
+
+X_train, X_test, y_train, y_test = train_test_split(pData.drop("Survived", axis=1), pData["Survived"], test_size=0.33,
+                                                    random_state=42)
+
+# Чтото полиномиальные фичи не на что не влияют(((
+# poly = PolynomialFeatures(2,include_bias=False)
+# A=poly.fit_transform(X_train)
+# X_train=pd.DataFrame(A)
+#
+# poly = PolynomialFeatures(2,include_bias=False)
+# A=poly.fit_transform(X_test)
+# X_test=pd.DataFrame(A)
+
+
+clf = DecisionTreeClassifier(random_state=False)
+clf.fit(X_train, y_train)
+
+train_result = clf.predict(X_train)
+target_names = ['Survived', 'Not survived']
+
+print("Train result")
+print(classification_report(y_train, train_result, target_names=target_names))
+print(accuracy_score(y_train, train_result))
+
+print("Test result")
+test_result = clf.predict(X_test)
+print(classification_report(y_test, test_result, target_names=target_names))
+print(accuracy_score(y_test, test_result))
--- utils/plotter.py
+++ utils/plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_predict_accuracity(predicted, true_values):
@@ -8,4 +9,20 @@ def plot_predict_accuracity(predicted, true_values):
             'k--', lw=4)
     ax.set_xlabel('Measured')
     ax.set_ylabel('Predicted')
+    plt.show()
+
+
+def plot_hist(X, bins=20):
+    plt.hist(X, bins=bins)
+    plt.show()
+
+
+def show_two_hist(x, y, bins=20):
+    plt.hist(x, bins, alpha=0.5, label='x')
+    plt.hist(y, bins, alpha=0.5, label='y')
+    plt.show()
+
+
+def show_two_plot(x1, y1, x2, y2):
+    plt.plot(x1, y1, 'bs', x2, y2, 'ro',alpha=0.5)
     plt.show()
