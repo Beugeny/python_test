7fb34f288db4c73612ec13479fe2b8e6eaf40954
Beugeny <b.ev@bk.ru>
2017-04-08 14:00:15 +0300

    .

--- Test1.py
+++ Test1.py
@@ -1,55 +1,65 @@
 import sys
+from PyQt5 import Qt
+
+from PyQt5 import QtCore
+import pandas as pd
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QStyleFactory
+from PyQt5.QtCore import QAbstractTableModel
+from PyQt5.QtCore import QStringListModel
+from PyQt5.QtCore import QVariant
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QMainWindow
+from spyder.widgets.variableexplorer.dataframeeditor import DataFrameModel
 
 import qdarkstyle
+from ui.TestUI import Ui_MainWindow
 
 
-class MyWindow(QtWidgets.QWidget):
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, parent=None, *args):
+        super(TableModel, self).__init__()
+        self.datatable = None
+
+    def update(self, dataIn):
+        self.datatable = dataIn
+
+    def rowCount(self, parent=QtCore.QModelIndex()):
+        return len(self.datatable.index)
+
+    def columnCount(self, parent=QtCore.QModelIndex()):
+        return len(self.datatable.columns.values)
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            i = index.row()
+            j = index.column()
+            return '{0}'.format(self.datatable.iget_value(i, j))
+        else:
+            return QtCore.QVariant()
+
+    def flags(self, index):
+        return QtCore.Qt.ItemIsEnabled
+
+
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
 
-    def on_btn_click(self):
-        self.l.setText("Я нажат, я нажат, очееееень длинный текст")
-
     def init_ui(self):
-        self.b = QtWidgets.QPushButton("Push button")
-        self.b.clicked.connect(self.on_btn_click)
-        self.l = QtWidgets.QLabel("Label")
+        w = Ui_MainWindow()
+        w.setupUi(self)
 
-        h_box = QtWidgets.QHBoxLayout()
-        h_box.addStretch()
-        h_box.addWidget(self.b)
-        h_box.addStretch()
-        h_box.addWidget(self.l)
-        h_box.addStretch()
-
-        v_box = QtWidgets.QVBoxLayout()
-        v_box.addLayout(h_box)
-
-        self.le = QtWidgets.QLineEdit()
-        self.le.textChanged.connect(lambda: self.b.setText(self.sender().text()))
-
-        self.s1 = QtWidgets.QSlider(Qt.Horizontal)
-        self.s1.setMinimum(1)
-        self.s1.setMaximum(100)
-        self.s1.setValue(25)
-        self.s1.setTickInterval(10)
-        self.s1.setTickPosition(QtWidgets.QSlider.TicksBelow)
-
-        v_box.addWidget(self.s1)
-        v_box.addWidget(self.le)
-
-        self.setLayout(v_box)
-        self.setWindowTitle('Заголовок окна')
+        m = TableModel()
+        m.update(pd.DataFrame({"A": [10, 20, 30, 40], "B": ["one", "two", "three", "four"]}))
+        w.tableView.setModel(m)
+        w.tableView_2.setModel(m)
 
 
 app = QtWidgets.QApplication(sys.argv)
 app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 w = MyWindow()
 w.show()
-
 
 sys.exit(app.exec_())
---
+++ ui/TestUI.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\resources\machineLearning\python_test\ui\TestUI.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(700, 604)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setObjectName("tableView")
+        self.verticalLayout.addWidget(self.tableView)
+        self.tableView_2 = QtWidgets.QTableView(self.centralwidget)
+        self.tableView_2.setObjectName("tableView_2")
+        self.verticalLayout.addWidget(self.tableView_2)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 700, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
